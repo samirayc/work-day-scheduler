c96f84082aa69b9be86a37ff2e6f1c518825f981
Samira Chetta <chettasamira@gmail.com>
2024-01-07 18:17:03 -0500

    JS for getting user input from local storage and for color-coding blocks according to current time

--- Develop/script.js
+++ Develop/script.js
@@ -2,14 +2,12 @@ var currentDayEl = $('#currentDay');
 var saveBtnEl = $('.saveBtn');
 var timeBlockEl = $('.time-block');
 var descriptionEl = $('.description');
+
+    // TODO: Add code to display the current date in the header of the page.
 var currentTime = dayjs().format('dddd, MMMM D, h:mm');
 currentDayEl.text(currentTime);
 
 
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
@@ -18,35 +16,69 @@ $(function () {
     // useful when saving the description in local storage?
 
     // Adding event listener
-   saveBtnEl.on('click', function (){
-    var appointmentCreatedEl = $('.appointment-created');
+    saveBtnEl.on('click', function () {
+        var appointmentCreatedEl = $('.appointment-created');
 
-    // Getting value of description box and the selected time
-    var textInput = $(this).siblings('description').val().trim();
-    var timeSelected = $(this).parent().attr('id');
+        // Getting value of description box and the selected time
+        var textInput = $(this).siblings('.description').val().trim();
+        var timeSelected = $(this).parent().attr('id');
 
-    // Saving to local storage
-    if (textInput === ""){
-        return;
-    } else {
-        localStorage.setItem(timeSelected, textInput);
-        appointmentCreatedEl.text("Appointment added to localStorage!");
+        // Saving to local storage
+        if (textInput === "") {
+            return;
+        } else {
+            localStorage.setItem(timeSelected, textInput);
+            appointmentCreatedEl.text("Appointment added to localStorage! ✔️");
 
-        setTimeout(function () {
-            appointmentCreatedEl.text("");
-        }, 3000)
+            setTimeout(function () {
+                appointmentCreatedEl.text("");
+            }, 3000)
         }
-   }) 
+    })
+
+
+
+    // TODO: Add code to get any user input that was saved in localStorage and set
+    // the values of the corresponding textarea elements. HINT: How can the id
+    // attribute of each time-block be used to do this?
+
+    // Get stored user input items from the local storage
+    $('#hour-9 .description').val(localStorage.getItem('hour-9'));
+    $('#hour-10 .description').val(localStorage.getItem('hour-10'));
+    $('#hour-11 .description').val(localStorage.getItem('hour-11'));
+    $('#hour-12 .description').val(localStorage.getItem('hour-12'));
+    $('#hour-13 .description').val(localStorage.getItem('hour-13'));
+    $('#hour-14 .description').val(localStorage.getItem('hour-14'));
+    $('#hour-15 .description').val(localStorage.getItem('hour-15'));
+    $('#hour-16 .description').val(localStorage.getItem('hour-16'));
+    $('#hour-17 .description').val(localStorage.getItem('hour-17'));
 
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
     // attribute of each time-block be used to conditionally add or remove the
     // past, present, and future classes? How can Day.js be used to get the
     // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    // TODO: Add code to display the current date in the header of the page.
-  });
-  
+
+    // Color-coding time blocks using past, present, and future classes
+    function currentTimeBlock() {
+        var currentHour = dayjs().hour();
+        var timeBlockEl = $('.time-block');
+        timeBlockEl.each(function () {
+            var currentTime = parseInt($(this).attr('id').split('-')[1]);
+
+            if (currentTime < currentHour) {
+                $(this).addClass('past');
+            } else if (currentTime === currentHour) {
+                $(this).removeClass('past');
+                $(this).removeClass('future');
+                $(this).addClass('present');
+            } else {
+                $(this).removeClass('past');
+                $(this).removeClass('present');
+                $(this).addClass('future');
+            }
+        })
+    }
+
+    currentTimeBlock();
+
